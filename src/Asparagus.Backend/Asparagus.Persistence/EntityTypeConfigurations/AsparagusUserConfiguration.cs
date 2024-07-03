54b2803d30a1e77b6e56a7af7e84060170336986
Ivan <zhmur.ivan@gmail.com>
2024-07-03 12:35:22 +0300

    chore(backend): split the database configuration classes into two files

---
+++ src/Asparagus.Backend/Asparagus.Persistence/EntityTypeConfigurations/AsparagusUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Asparagus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Asparagus.Persistence.EntityTypeConfigurations;
+
+public class AsparagusUserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.HasMany(u => u.UserEatings)
+            .WithOne(ue => ue.User)
+            .HasForeignKey(ue => ue.UserId);
+    }
+}
+
--- src/Asparagus.Backend/Asparagus.Persistence/EntityTypeConfigurations/AsparagusUserEatingConfiguration.cs
+++ src/Asparagus.Backend/Asparagus.Persistence/EntityTypeConfigurations/AsparagusUserEatingConfiguration.cs
@@ -4,18 +4,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Asparagus.Persistence.EntityTypeConfigurations;
 
-public class AsparagusUserConfiguration : IEntityTypeConfiguration<User>
-{
-    public void Configure(EntityTypeBuilder<User> builder)
-    {
-        builder.HasKey(u => u.Id);
-
-        builder.HasMany(u => u.UserEatings)
-            .WithOne(ue => ue.User)
-            .HasForeignKey(ue => ue.UserId);
-    }
-}
-
 public class AsparagusUserEatingConfiguration : IEntityTypeConfiguration<UserEating>
 {
     public void Configure(EntityTypeBuilder<UserEating> builder)
